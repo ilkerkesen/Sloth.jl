8fa317dbfda52ae3bf454e47fc427b12ca2ec8d2
Ilker Kesen <ilkerksn@gmail.com>
2019-06-03 16:11:19 +0000

    fix initialization type issue

--- src/init.jl
+++ src/init.jl
@@ -7,7 +7,7 @@ end
 
 
 function initwb(input_dim::Int, output_dim::Int, atype=_atype, init=xavier, bias=true)
-    w = param(output_dim, input_dim; init=init, atype=_atype)
-    b = bias ? param(output_dim, 1; atype=_atype) : eltype(w)(0)
+    w = param(output_dim, input_dim; init=init, atype=atype)
+    b = bias ? param(output_dim, 1; atype=atype) : eltype(w)(0)
     return (w, b)
 end
