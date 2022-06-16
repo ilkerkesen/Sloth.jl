4412fdb4c0ce5bd7584bd11d85e8c5d3a4c52ed1
Ilker Kesen <ilkerksn@gmail.com>
2022-06-17 02:39:50 +0300

    adaptation to the most recent Knet version

--- src/Sloth.jl
+++ src/Sloth.jl
@@ -7,7 +7,7 @@ import Knet.Ops20: BNMoments
 import Knet: atype
 
 
-const SlothArray = Union{Array{T}, KnetArray{T}} where T <: AbstractFloat
+const SlothArray = Union{Array{T}, KnetArray{T}, CuArray{T}} where T <: AbstractFloat
 const SlothParam = Param{T} where T <: SlothArray
 const SlothWeight = Union{SlothArray, SlothParam}
 const SlothBias = Union{SlothArray, SlothParam, AbstractFloat}
